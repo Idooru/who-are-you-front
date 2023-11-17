77d575105b75c5dd8f13a4071ce2ebc22969e88f
idooru <shere1765@gmail.com>
2023-11-17 22:53:50 +0900

    [수정] 파일 경로

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  const title = 'who are you';
+
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+    </div>
+  );
+}
+
+export default App;
