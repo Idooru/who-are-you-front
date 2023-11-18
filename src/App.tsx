e4ec2e0c8f9fe81568341a14447b36ce6a5666e9
idooru <shere1765@gmail.com>
2023-11-18 16:40:32 +0900

    [수정] 레이아웃 컴포넌트 호출

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Layout from './components/home/layout/Layout';
 
 function App() {
-  const title = 'who are you';
-
   return (
     <div className="App">
-      <h1>{title}</h1>
+      <Layout />
     </div>
   );
 }
