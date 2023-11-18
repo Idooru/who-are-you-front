a7546479d53acfeb7054258d9f49b36d4eef11fd
idooru <shere1765@gmail.com>
2023-11-18 16:39:51 +0900

    [추가] 오른쪽 라인 컴포넌트 호출

--- src/components/home/layout/Layout.tsx
+++ src/components/home/layout/Layout.tsx
@@ -1,14 +1,16 @@
 import layoutStyle from './Layout.module.css';
 import Title from '../title/Title';
 import Login from '../login/Login';
+import SubTitle from '../subtitle/SubTitle';
 
 const Layout = () => {
   return (
     <div className={layoutStyle.area}>
-      {/*<div className={`${layoutStyle.box} ${layoutStyle.left}`}>left</div>*/}
-      {/*<div className={`${layoutStyle.box} ${layoutStyle.right}`}>right</div>*/}
       <Title />
-      <Login />
+      <div className={layoutStyle.right}>
+        <SubTitle />
+        <Login />
+      </div>
     </div>
   );
 };
