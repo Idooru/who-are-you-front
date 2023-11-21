d2480505a8cddc517f2ac986c8aef2dced4c15be
idooru <shere1765@gmail.com>
2023-11-21 23:51:39 +0900

    [추가] 이메일, 비밀번호 검증하기

--- src/components/home/login/Login.tsx
+++ src/components/home/login/Login.tsx
@@ -8,18 +8,34 @@ import React, { useEffect, useRef, useState } from 'react';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(false);
+  const [notAllow, setNotAllow] = useState(true);
+
   const emailInputRef = useRef<HTMLInputElement | null>(null);
 
   useEffect(() => {
     emailInputRef.current?.focus();
   }, []);
 
+  useEffect(() => {
+    isValidEmail && isValidPassword ? setNotAllow(false) : setNotAllow(true);
+  }, [isValidEmail, isValidPassword]);
+
   const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    regex.test(email) ? setIsValidEmail(true) : setIsValidEmail(false);
   };
 
   const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
+    const regex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    regex.test(password) ? setIsValidPassword(true) : setIsValidPassword(false);
   };
 
   const onSubmit = (e: React.FormEvent<HTMLInputElement>) => {
@@ -49,6 +65,11 @@ const Login = () => {
               ref={emailInputRef}
               autoComplete={'off'}
             />
+            {!isValidEmail && email.length > 0 && (
+              <div className={'error_box'}>
+                Please format your email correctly
+              </div>
+            )}
             <label htmlFor={'password_input'}>
               <FontAwesomeIcon icon={faLock} className={loginStyle.icons} />
             </label>
@@ -60,6 +81,11 @@ const Login = () => {
               onChange={onChangePassword}
               autoComplete={'off'}
             />
+            {!isValidPassword && password.length > 0 && (
+              <div className={'error_box'}>
+                Please format your password correctly
+              </div>
+            )}
             <div id={loginStyle.remember_user}>
               <input type={'checkbox'} />
               <p
@@ -70,6 +96,7 @@ const Login = () => {
               </p>
             </div>
             <input
+              disabled={notAllow}
               className={'submit_button sort_box none_line'}
               type={'submit'}
               value={'LOGIN'}
